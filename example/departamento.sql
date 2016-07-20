1faa79b9b6ff7c665f62d36b199c447ba4283a6f
Forduz <axidsugar@gmail.com>
2016-07-20 16:22:11 -0500

    nuevo archivo gitignore

---
+++ example/departamento.sql
@@ -0,0 +1,37 @@
+/*SELECT CONCAT('INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (',departamento_id,',\'',nombre,'\',\'',codigo,'\'));') FROM `departamento`;
+*/
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (101,'ANTIOQUIA','05');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (102,'ATLANTICO','08');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (103,'BOGOTA','11');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (104,'BOLIVAR','13');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (105,'BOYACA','15');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (106,'CALDAS','17');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (107,'CAQUETA','18');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (108,'CAUCA','19');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (109,'CESAR','20');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (110,'CORDOBA','23');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (111,'CUNDINAMARCA','25');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (112,'CHOCO','27');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (113,'HUILA','41');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (114,'LA GUAJIRA','44');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (115,'MAGDALENA','47');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (116,'META','50');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (117,'NARIÑO','52');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (118,'NORTE DE SANTANDER','54');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (119,'QUINDIO','63');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (120,'RISARALDA','66');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (121,'SANTANDER','68');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (122,'SUCRE','70');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (123,'TOLIMA','73');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (124,'VALLE DEL CAUCA','76');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (125,'ARAUCA','81');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (126,'CASANARE','85');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (127,'PUTUMAYO','86');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (128,'SAN ANDRES','88');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (129,'AMAZONAS','91');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (130,'GUAINIA','94');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (131,'GUAVIARE','95');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (132,'VAUPES','97');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (133,'VICHADA','99');
+INSERT INTO `departamento` (`departamento_id`,`nombre`,`codigo`) VALUES (134,'layneTest','0205');
+
